95b0d2402c980edee87b2874ec62541e3d0d9ae4
raul <sarmancoder@gmail.com>
2022-10-10 00:14:34 +0200

    Provocar error

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -14,6 +14,7 @@ const handler = nc<NextApiRequest, NextApiResponse>({
 })
     .get(async (req, res) => {
         console.log('HOLA MUNDO, AQUI LISTADO DE TODOS')
+        throw new Error('NO SE PUEDE CONSEGUIR TODOTEXT DE UNDEFINED')
 
         res.status(200).json({
             todos: await listTodos()
